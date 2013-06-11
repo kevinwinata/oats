54332d353bc482556b74334aa7519c9a372622eb
kevinwinata <kevinwinata@gmail.com>
2013-06-11 16:50:39 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oats::Application.config.secret_token = '3a426498156c3b06ea9fd97d649a8f873e3dfd6289b211083c75671227467d12528ad0e80004754b365433162a7eb4914df175264ae30c468c1f6736bc2e8735'

718974ab05cad0c52b6a067c5d13c55b60673560
wilsonfonda <wilsonfonda@gmail.com>
2013-06-13 11:15:09 +0700

    Starting controller company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,11 +3,20 @@ class CompaniesController < ApplicationController
   include ActionController::Helpers
   include ActionController::Cookies
   
+  def new
+  end
+
+  def create
+  @company = Company.new(params[:company])
+  @company.save
+end
+  end
+
   def index
   	@companies = Company.all
   	respond_to do |format|
   		format.html
-  		format.json { render }
+  		format.json
   	end
   end
 

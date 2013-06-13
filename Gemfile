8756ed8fe9d2fa65ab4e19c0e3e953b1071517d7
wilsonfonda <wilsonfonda@gmail.com>
2013-06-13 09:06:26 +0700

    starting rails API

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'faker', '1.0.1'
 gem 'will_paginate', '3.0.3'
 gem 'bootstrap-will_paginate', '0.0.6'
 gem 'json_spec'
+gem 'rails-api'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,8 @@ module Oats
     # Enable the asset pipeline
     config.assets.enabled = true
 
+	config.api_only = false
+	
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end

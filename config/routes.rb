fe4c9c81eb355484ecc4eb5cdb096919fdee4f41
janicelaksana <13510035@std.stei.itb.ac.id>
2013-06-13 19:33:10 -0700

    All static pages and client testimony done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Oats::Application.routes.draw do
+  get "static_pages/about"
+  get "static_pages/contact"
+  get "static_pages/terms"
+  get "static_pages/privacy"
+  get "static_pages/clientsandtestimony"
+  get "static_pages/help"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "About page" do
+  	it "should have the content 'About Us'" do
+  		visit '/static_pages/about'
+  		page.should have_content('About Us')
+  	end
+  end
+
+  describe "Contact page" do
+  	it "should have the content 'Contact Us'" do
+  		visit '/static_pages/contact'
+  		page.should have_content('Contact Us')
+  	end
+  end
+
+  describe "Terms of Service page" do
+    it "should have the content 'Terms of Service'" do
+      visit '/static_pages/terms'
+      page.should have_content('Terms of Service')
+    end
+  end
+
+  describe "Privacy of Policy page" do
+    it "should have the content 'Privacy of Policy'" do
+      visit '/static_pages/privacy'
+      page.should have_content('Privacy of Policy')
+    end
+  end
+
+  describe "Clients & Testimony page" do
+    it "should have the content 'Clients & Testimony'" do
+      visit '/static_pages/clientsandtestimony'
+      page.should have_content('Clients and Testimony')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+end

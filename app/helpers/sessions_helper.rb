2b106a31682aff05ef1b3ff721c298ae556e6432
wilsonfonda <wilsonfonda@gmail.com>
2013-06-14 14:22:56 +0700

    sessions_helper => employee_sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,26 +1,4 @@
-module SessionsHelper
-
-  def sign_in(company)
-    cookies.permanent[:remember_company] = company.remember_company
-    self.current_company = company
-  end
-
-  def signed_in?
-    !current_company.nil?
-  end
-
-  def current_company=(company)
-    @current_company = company
-  end
-
-  def current_company
-    @current_company ||= Company.find_by_remember_company(cookies[:remember_company])
-  end
-
-  def sign_out
-    self.current_company = nil
-    cookies.delete(:remember_company)
-  end
+module EmployeeSessionsHelper
 
   def employee_sign_in(employee)
     cookies.permanent[:remember_token] = employee.remember_token

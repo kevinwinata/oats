7945e089b86644d2a04ed19f0c11155880da0f25
janicelaksana <13510035@std.stei.itb.ac.id>
2013-06-17 00:47:44 -0700

    Adding stopwatch to Employee Home Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,52 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require_tree 
+
+var interval;
+
+function startCount(){
+	var second = parseInt(document.getElementById("timer-second").innerHTML, 10);
+	var minute = parseInt(document.getElementById("timer-minute").innerHTML, 10);
+	var hour = parseInt(document.getElementById("timer-hour").innerHTML, 10);
+	if (second < 59){
+		second++;
+	} else{
+		second = 0;
+		if (minute < 59) {
+			minute++;
+		}else{
+			minute = 0;
+			if (hour < 59) {
+				hour++;
+			}
+		}
+	}
+
+	document.getElementById("timer-second").innerHTML = padding(second.toString());
+	document.getElementById("timer-minute").innerHTML = padding(minute.toString());
+	document.getElementById("timer-hour").innerHTML = padding(hour.toString());
+}
+
+function start(){
+	document.getElementById("timer-second").innerHTML = "00";
+	document.getElementById("timer-minute").innerHTML = "00";
+	document.getElementById("timer-hour").innerHTML = "00";
+	interval = setInterval(function() {startCount();}, 1000);
+	document.getElementById("checkin").style.visibility = 'hidden';
+	document.getElementById("checkout").style.visibility = 'visible';
+}
+
+function stop(){
+	clearInterval(interval);
+	document.getElementById("checkin").style.visibility = 'visible';
+	document.getElementById("checkout").style.visibility = 'hidden';
+}
+
+function padding(number){
+	while(number.length < 2){
+			number = '0' + number;
+	}
+
+	return number;
+}
